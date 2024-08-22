c7093c501e563e12cdd7baf7801e98a0d3a2ddf2
blayne-wesneski <102747184+blayne-wesneski@users.noreply.github.com>
2024-08-22 17:51:57 -0400

    Create method to check if array is empty

--- Arrays/Activity0.java
+++ Arrays/Activity0.java
@@ -2,20 +2,29 @@ package Arrays;
 
 public class Activity0 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        int[] numbers = {2, 4, 6, 8, 10};
+        int[] numbers = { 2, 4, 6, 8, 10 };
 
         print_array(numbers);
+
+        System.out.println(isEmpty(numbers));
+
     }
 
-    public static void print_array(int[] array){
+    // print array values
+    public static void print_array(int[] array) {
         System.out.print("[");
 
         for (int i = 0; i < array.length - 1; i++) {
-            System.out.print(array[i] + ", ");           
+            System.out.print(array[i] + ", ");
         }
 
         System.out.println(array[array.length - 1] + "]");
+    }
+
+    // check if the array is empty
+    public static boolean isEmpty(int[] array) {
+        return array.length == 0;
     }
 }
